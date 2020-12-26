acd176d1d85b1670512391c00825d995903139bb
unknown <s.kamilriyas@gmail.com>
2020-12-26 10:25:27 +0530

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<main>
+	<h1>Hello World!🤗</h1>
+	<p>Site is currently under construction.🙄</p>
+	<p>Being built using Svelte</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
